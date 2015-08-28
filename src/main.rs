844085250a8c8ef17ad10a2de3b936fb3b3f0e45
Richard Fontana <fontana@sharpeleven.org>
2015-08-27 22:47:52 -0400

    Add function to get version from Cargo metadata

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::fs;
 
 fn main() {
-
     match fs::create_dir("_site") {
         Ok(_) => (),
         Err(_) => (),
     }
-
 }
 
-
+fn version() -> &'static str {
+    return env!("CARGO_PKG_VERSION");
+}
